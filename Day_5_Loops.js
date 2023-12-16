52cfc9b669a1d5f560806bef382dc5fdc5689418
gu1tekin <gultekincirik@gmail.com>
2023-12-16 23:24:47 +0100

    day 5 - passed

---
+++ Day_5_Loops.js
@@ -0,0 +1,30 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function (inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+    //Implemented a for loop and utilized template literals to display the output.
+    for (let i = 1; i <= 10; i++) {
+        let result = n * i;
+        console.log(`${n} x ${i} = ${result}`)
+    }
+}
