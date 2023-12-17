a213287df3d5c27c0b85f545571e599484f6b0d4
gu1tekin <gultekincirik@gmail.com>
2023-12-18 00:31:26 +0100

    day 6 - passed

--- Day_6_Lets_Review.js
+++ Day_6_Lets_Review.js
@@ -1,6 +1,31 @@
 function processData(input) {
-    //Enter your code here
-} 
+    // Split the input into an array of lines
+    const inputArray = input.split('\n');
+
+    // Get the number of test cases (the first element in the array)
+    const numTestCases = parseInt(inputArray[0]);
+
+    // Iterate over the test cases
+    for (let i = 1; i <= numTestCases; i++) {
+        // Get the current test case
+        const currentTestCase = inputArray[i];
+
+        // Separate even-indexed and odd-indexed characters
+        let evenChars = '';
+        let oddChars = '';
+
+        for (let x = 0; x < currentTestCase.length; x++) {
+            if (x % 2 === 0) {
+                evenChars += currentTestCase[x];
+            } else {
+                oddChars += currentTestCase[x];
+            }
+        }
+
+        // Print the result
+        console.log(`${evenChars} ${oddChars}`);
+    }
+}
 
 process.stdin.resume();
 process.stdin.setEncoding("ascii");
@@ -10,5 +35,5 @@ process.stdin.on("data", function (input) {
 });
 
 process.stdin.on("end", function () {
-   processData(_input);
+    processData(_input);
 });
